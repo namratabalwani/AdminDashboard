19423b54033147718b6173fafdfea641fcea1b8e
namrata <namratabalwani1@gmail.com>
2024-07-04 11:51:28 -0400

    Changes in Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -27,10 +27,122 @@ const Sidebar = ({
     useEffect(() => {
         setActive(pathName.substring(1));
     }, [pathName]);
-    
+
+    const navItems = [
+        {
+            text: "Dashboard",
+            icon: <HomeOutlined/>
+        },
+        {
+            text: "client Facing",
+            icon: null
+        },
+        {
+            text: "Products",
+            icon: <ShoppingCartOutlined/>
+        },
+        {
+            text: "Customers",
+            icon: <Groups2Outlined/>
+        },
+        {
+            text: "Transactions",
+            icon: <RecieptLongOutlined/>
+        },
+        {
+            text: "Geography",
+            icon: <PublicOutlined/>
+        },
+        {
+            text: "Sales",
+            icon: null
+        },  
+        {
+            text: "Overviewe",
+            icon: <PointOfSaleOutlined/>
+        },
+        {
+            text: "Daily",
+            icon: <TodayOutlined/>
+        },
+        {
+            text: "Monthly",
+            icon: <CalendarMonthOutlined/>
+        },
+        {
+            text: "BreakDown",
+            icon: <PieChartOutlined/>
+        },
+        {
+            text: "Management",
+            icon: null        },
+        {
+            text: "Admin",
+            icon: <AdminPanelSettingsOutlined/>
+        },
+        {
+            text: "Performance",
+            icon: <TrendingUpOutlined/>
+        },
+    ]
   return (
     <Box component="nav">
-        
+        {isSidebarOpen && (
+            <Drawer open={isSidebarOpen}
+                    onClose={() => setIsSidebarOpen(false)}
+                    variant='persistent'
+                    anchor='left'
+                    sx={{
+                        width: drawerWidth,
+                        "& .MuiDrawer-paper": {
+                            color: theme.palette.secondary[200],
+                            backgroundColor: theme.palette.background.alt,
+                            boxSizing: "border-box",
+                            borderWidth: isNonMobile ? 0 : "2px" ,
+                            width: drawerWidth
+                        }
+
+                    }}>
+                        <Box width="100%">
+                            <Box m="1.5rem 2rem  2rem 3 rem">
+                                <FlexBetween color={theme.palette.secondary.main}>
+                                    <Box display="flex" alignItems="center" gap="0.5rem">
+                                        <Typography variant='h4' fontWeight="bold">
+                                            ECOMVISION
+                                        </Typography>
+                                    </Box>
+                                    {!isNonMobile && (
+                                        <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                                            <ChevronLeft />
+                                        </IconButton>
+                                    )}
+                                </FlexBetween>
+                            </Box>
+                            <List>
+                                {
+                                    navItems.map(({text, icon})  => {
+                                        if(!icon)  {
+                                            return (
+                                            <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem"}}>
+                                                {text}
+                                            </Typography>
+                                        )}
+                                        const lcText = text.toLowerCase();
+                                        return (
+                                            <ListItem key={text} disablePadding>
+                                                <ListItemButton onClick={() => {navigate(`/${lcText}`);
+                                                                                setActive(lcText);}}></ListItemButton>
+
+                                            </ListItem>
+                                        )
+                                    }
+
+                                )}
+                            </List>
+
+                        </Box>
+                    </Drawer>
+        )}
 
     </Box>
   )
